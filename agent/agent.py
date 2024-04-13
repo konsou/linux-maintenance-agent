8a7a25cc79453c9f582f680b3ff834c2cb9462cf
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-13 20:21:45 +0300

    Refactor

--- agent/agent.py
+++ agent/agent.py
@@ -12,25 +12,24 @@ from text import print_in_color, Color
 class Agent:
     def __init__(self, system_prompt: str = settings.LLM_SYSTEM_PROMPT):
         self.api: LlmApi = settings.LLM_API
+        self.chat_history: list[types_request.Message] = []
+
         self.start_greeting = "Hello! How can I help you today?"
-        self.system_message: types_request.Message = types_request.Message(
+
+        self.system_prompt = system_prompt
+        self.add_to_chat_history(
             content=system_prompt,
             role="system",
         )
-        self._chat_history: list[types_request.Message] = [self.system_message]
 
-        self._system_info: dict = self.gather_system_info()
-        if self._system_info:
-            self._chat_history.append(
-                types_request.Message(
-                    content=f"Here's basic information on the user's system:\n"
-                    + json.dumps(self._system_info, indent=2),
-                    role="system",
-                )
+        self.system_info: dict = self.gather_system_info()
+        if self.system_info:
+            self.add_to_chat_history(
+                content=f"Here's basic information on the user's system:\n"
+                + json.dumps(self.system_info, indent=2),
+                role="system",
             )
-        self._chat_history.append(
-            types_request.Message(content=self.start_greeting, role="assistant")
-        )
+        self.add_to_chat_history(content=self.start_greeting, role="assistant")
 
         self.tools = tools.tools
 
@@ -40,11 +39,10 @@ class Agent:
         message_role: types_request.MessageRole = "user",
         allow_tools: bool = True,
     ) -> str:
-        self._chat_history.append(
-            types_request.Message(content=message, role=message_role)
-        )
+        self.add_to_chat_history(content=message, role=message_role)
+
         response: str | types_tools.ToolCall = self.api.response_from_messages(
-            self._chat_history, tools=self.tools if allow_tools else None
+            self.chat_history, tools=self.tools if allow_tools else None
         )
 
         # Is a tool call
@@ -54,19 +52,46 @@ class Agent:
 
         return self.handle_string_response(response)
 
+    def add_to_chat_history(self, content: str, role: types_request.MessageRole):
+        new_message = types_request.Message(content=content, role=role)
+        self._add_or_merge_message(new_message)
+
+    def _add_or_merge_message(self, message: types_request.Message):
+        if len(self.chat_history) == 0:
+            self.chat_history.append(message)
+            return
+
+        roles_to_merge = ("user", "assistant")
+        latest_message = self.chat_history[-1]
+        if (
+            message["role"] in roles_to_merge
+            and message["role"] == latest_message["role"]
+        ):
+            print(
+                f"Duplicate roles \"{message['role']}\" detected, merging messages..."
+            )
+            latest_message["content"] = (
+                f"{latest_message['content']}\n{message['content']}"
+            )
+            return
+        self.chat_history.append(message)
+
     def handle_tool_call(self, tool_call: types_tools.ToolCall) -> str:
         message_content = json.dumps(tool_call, indent=2)
-        self._chat_history.append(
-            types_request.Message(content=message_content, role="assistant")
+        self.add_to_chat_history(content=message_content, role="assistant")
+
+        tool_result = f"Output and exit code for this tool call:\n---\n{self.run_tool(tool_call)}\n---"
+        self.add_to_chat_history(content=tool_result, role="assistant")
+
+        tool_use_response = self.get_response(
+            "Please explain your action and the results",
+            message_role="user",
+            allow_tools=False,
         )
-        tool_result = f"Tool use result:\n{self.run_tool(tool_call)}"
-        tool_use_response = self.get_response(tool_result, message_role="system")
         return tool_use_response
 
     def handle_string_response(self, response: str) -> str:
-        self._chat_history.append(
-            types_request.Message(content=response, role="assistant")
-        )
+        self.add_to_chat_history(content=response, role="assistant")
         return response
 
     def run_tool(self, tool_call: types_tools.ToolCall) -> str:
