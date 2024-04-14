b9e86802d71ee113ff21e9af97069878211e7ea9
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-14 21:00:07 +0300

    Make compatible with python 3.11

--- agent/tools.py
+++ agent/tools.py
@@ -17,7 +17,8 @@ tools = [
             "description": (
                 "Runs a command line command on the user's system. The user is asked for their consent "
                 "before executing the command. RUN ONLY CLI COMMANDS THAT OUTPUT TEXT.\n\n"
-                "On Windows this uses PowerShell. On Linux the default shell is used."),
+                "On Windows this uses PowerShell. On Linux the default shell is used."
+            ),
             "parameters": {
                 "type": "object",
                 "properties": {
@@ -38,11 +39,14 @@ def run_command_line(command: str, timeout: float = 30, *args, **kwargs) -> str:
     if platform.system() == "Windows":
         # Command to invoke PowerShell on Windows
         # Also escape double quotes
-        command = f'powershell -Command "{command.replace('"', '\\"')}"'
+        command = command.replace('"', '\\"')
+        command = f'powershell -Command "{command}"'
 
     print(f'Running command "{command}"...')
 
-    def _format_and_print_output(output: str, exit_code: int, color: str = text.Fore.WHITE) -> str:
+    def _format_and_print_output(
+        output: str, exit_code: int, color: str = text.Fore.WHITE
+    ) -> str:
         output = output.strip() if output.strip() else "(no output)"
         text_ = f"{output}\nProcess exited with code {exit_code}"
         print_in_color(text_, color=color)
@@ -59,12 +63,14 @@ def run_command_line(command: str, timeout: float = 30, *args, **kwargs) -> str:
             timeout=timeout,
         )
 
-        return _format_and_print_output(output=result.stdout,
-                                        exit_code=result.returncode,
-                                        color=Color.GREEN)
+        return _format_and_print_output(
+            output=result.stdout, exit_code=result.returncode, color=Color.GREEN
+        )
 
     except subprocess.CalledProcessError as e:
-        return _format_and_print_output(output=e.stdout, exit_code=e.returncode, color=Color.RED)
+        return _format_and_print_output(
+            output=e.stdout, exit_code=e.returncode, color=Color.RED
+        )
     except subprocess.TimeoutExpired as e:
         return _format_and_print_output(output=str(e), exit_code=1, color=Color.RED)
 
