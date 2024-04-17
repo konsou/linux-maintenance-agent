055cd7ddf7c994dfcde253fa776882d38a987126
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-17 19:52:57 +0300

    Change to haiku

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ from llm_api import OpenRouterAPI
 
 ALWAYS_SEND_SYSTEM_DATA = True
 # LLM_MODEL = "google/gemini-pro"
-LLM_MODEL = "anthropic/claude-3-sonnet:beta"
+LLM_MODEL = "anthropic/claude-3-haiku:beta"
+# LLM_MODEL = "anthropic/claude-3-sonnet:beta"
 # LLM_MODEL = "anthropic/claude-3-opus:beta"
 # LLM_MODEL = "cohere/command-r-plus"
 # LLM_MODEL = "openai/gpt-4-turbo"
-# "anthropic/claude-3-haiku:beta"
 LLM_API = OpenRouterAPI(model=LLM_MODEL)
