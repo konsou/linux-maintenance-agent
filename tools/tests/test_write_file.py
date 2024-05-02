ca04f77bf8e6afb1e53b43990d77891a5199079e
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-05-02 14:20:19 +0300

    Fix write_file not using work dir

--- tools/tests/test_write_file.py
+++ tools/tests/test_write_file.py
@@ -1,4 +1,5 @@
 import os
+from unittest.mock import patch
 
 from pyfakefs.fake_filesystem_unittest import TestCase
 
@@ -24,3 +25,26 @@ class TestWriteFile(TestCase):
         with open(filename, "r") as f:
             file_contents = f.read()
         self.assertEqual(contents, file_contents, "File contents do not match")
+
+    def test_write_file_uses_default_work_dir(self):
+        work_dir = "/home/test/workdir"
+        test_file_name = "test_file.txt"
+        self.fs.create_dir(work_dir)
+        with patch("settings.AGENT_WORK_DIR", work_dir):
+            tools.write_file.write_file(test_file_name, "Hello World")
+        self.assertTrue(self.fs.isfile(os.path.join(work_dir, test_file_name)))
+
+    def test_write_file_uses_argument_work_dir(self):
+        work_dir_argument = "/home/test/argument-workdir"
+        work_dir_settings = "/home/test/workdir-in-settings"
+        test_file_name = "test_file.txt"
+        self.fs.create_dir(work_dir_argument)
+        self.fs.create_dir(work_dir_settings)
+        with patch("settings.AGENT_WORK_DIR", work_dir_settings):
+            tools.write_file.write_file(
+                test_file_name, "Hello World", work_dir=work_dir_argument
+            )
+        self.assertTrue(self.fs.isfile(os.path.join(work_dir_argument, test_file_name)))
+        self.assertFalse(
+            self.fs.isfile(os.path.join(work_dir_settings, test_file_name))
+        )
--- tools/write_file.py
+++ tools/write_file.py
@@ -1,10 +1,19 @@
+import os.path
+
+import settings
+from text import print_in_color, Color
 from .consent import ask_execution_consent
 
 
 @ask_execution_consent
-def write_file(filename: str, content: str) -> str:
+def write_file(filename: str, content: str, work_dir: str | None = None) -> str:
+    if work_dir is None:
+        work_dir = settings.AGENT_WORK_DIR
+
+    full_path = os.path.join(work_dir, filename)
+    print_in_color(f"Writing file {full_path}", Color.LIGHTBLACK_EX)
     try:
-        with open(filename, "w", encoding="utf-8") as f:
+        with open(full_path, "w", encoding="utf-8") as f:
             f.write(content)
     except OSError as e:
         return f"Cannot write file {filename}: {e}"
