36b520fd2329e3462cbeb0ad3d9ba59e43abd5d3
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-07-01 19:28:17 +0300

    Add ToolPlan

---
+++ tools/plan.py
@@ -0,0 +1,27 @@
+import json
+
+from tools.abc import Tool, ToolProperty
+
+
+class ToolPlan(Tool):
+    def __init__(self):
+        super().__init__(
+            name="update_plan",
+            description="Create or update your plan on accomplishing your main goal.\nIMPORTANT! You should update your plan every time you've taken another action.",
+            properties={
+                "main_goal": ToolProperty(
+                    type="string",
+                    description="Short description of your main goal",
+                ),
+                "steps": ToolProperty(
+                    type="string",
+                    description="The steps needed to reach the goal. NOTE: For each step, you must indicate if it's [DONE], [IN PROGRESS], or [TODO]. List each step on its own line.",
+                ),
+            },
+            required=["main_goal", "content"],
+            callable=plan,
+        )
+
+
+def plan(main_goal: str, steps: str, *args, **kwargs) -> str:
+    return json.dumps({"main_goal": main_goal, "steps": steps})
